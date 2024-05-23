d203b0a7932c39a073f2aa3dc47fd2ce9b041c65
abhisheknaik24 <development.abhi2000@gmail.com>
2024-05-23 18:58:02 +0530

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import QueryProvider from '@/components/providers/query-provider';
 import { ToasterProvider } from '@/components/providers/toaster-provider';
 import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Nunito } from 'next/font/google';
 
 const font = Nunito({ subsets: ['latin'] });
@@ -11,8 +11,27 @@ const font = Nunito({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Handy Cricket',
   description: 'Handy Cricket',
+  generator: 'Handy Cricket',
+  manifest: '/manifest.json',
   keywords: ['Handy Cricket', 'Abhishek Naik'],
   authors: [{ name: 'Abhishek Naik' }],
+  icons: [
+    { rel: 'apple-touch-icon', url: 'icons/icon-128x128.png' },
+    { rel: 'icon', url: 'icons/icon-128x128.png' },
+  ],
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: 'cover',
+  colorScheme: 'dark',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: '#0ea5e9' },
+    { media: '(prefers-color-scheme: dark)', color: '#0ea5e9' },
+  ],
 };
 
 type Props = {
--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
+import nextPWA from 'next-pwa';
+
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     remotePatterns: [
       {
@@ -8,6 +12,16 @@ const nextConfig = {
       },
     ],
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
 };
 
-export default nextConfig;
+const withPWA = nextPWA({
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+});
+
+export default withPWA(nextConfig);
